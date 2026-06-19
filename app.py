a6963dd37cda04c4e211e29a144457ff9e56e382
ManasviBohra <manasvibohra8@gmail.com>
2026-06-20 02:39:25 +0530

    add gitignore

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
@@ -8,7 +12,7 @@ CORS(app)
 # MySQL configuration
 app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"        # your MySQL username
-app.config["MYSQL_PASSWORD"] = "mysql"        
+app.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")  
 app.config["MYSQL_DB"] = "home_organiser"
 
 mysql = MySQL(app)
